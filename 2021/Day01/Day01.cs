9e89b615139c1e13667ffdd02196465d05ff2008
madhurabhat1991 <madhurabhat1991@gmail.com>
2022-10-27 16:16:42 -0500

    Added foundation for Day 01

--- 2021/Day01/Day01.cs
+++ 2021/Day01/Day01.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Template;
 
 namespace _2021.Day01
 {
-    class Day01
+    public class Day01 : IDay<List<int>, long>
     {
+        public List<int> Example { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<int> Input { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Main(string[] example, string[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long PartOne(List<int> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long PartTwo(List<int> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> ProcessInput(string[] input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- 2021/Program.cs
+++ 2021/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace _2021
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ex = "";
+
+            var challenge = new Day01.Day01();
+            var day = "01";
+
+
+            var example = File.ReadAllLines($"Day{day}\\example{ex}.txt");
+            var input = File.ReadAllLines($"Day{day}\\input.txt");
+            challenge.Main(example, input);
         }
     }
 }
