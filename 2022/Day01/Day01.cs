936039fe7358c3a72854e2b55b9a4f64706aadb3
madhurabhat1991 <madhurabhat1991@gmail.com>
2022-11-28 16:28:01 -0600

    2022

---
+++ 2022/Day01/Day01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Template;
+
+namespace _2022.Day01
+{
+    public class Day01 : Day<List<String>, long, long>
+    {
+        public override string DayNumber { get { return "01"; } }
+
+        public override long PartOne(List<string> input)
+        {
+            return 0;
+        }
+
+        public override long PartTwo(List<string> input)
+        {
+            return 0;
+        }
+
+        public override List<string> ProcessInput(string[] input)
+        {
+            return input.ToList();
+        }
+    }
+}
