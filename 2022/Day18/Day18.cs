12aefeff0b342780b42de064d24565be815675a5
madhurabhat1991 <madhurabhat1991@gmail.com>
2022-12-05 22:11:57 -0600

    Added files

---
+++ 2022/Day18/Day18.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2022.Day18
+{
+    class Day18
+    {
+    }
+}
---
+++ 2022/Day23/Day23.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2022.Day23
+{
+    class Day23
+    {
+    }
+}
