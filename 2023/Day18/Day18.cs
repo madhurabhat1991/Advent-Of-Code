646d7c9e56fa014e93a4d277650de88dbd88ccbe
madhurabhat1991 <madhurabhat1991@gmail.com>
2023-11-23 13:15:37 -0600

    2023

---
+++ 2023/Day18/Day18.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Template;
+
+namespace _2023.Day18
+{
+    public class Day18 : Day<List<long>, long, long>
+    {
+        public override string DayNumber { get { return "18"; } }
+
+        public override long PartOne(List<long> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long PartTwo(List<long> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<long> ProcessInput(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
